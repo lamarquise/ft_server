cc1d565af912de1539d745a8d1a6e916b82026f0
lamarquise <e.lazo14@eabjm.org>
2020-11-18 15:59:35 +0100

    should be done

--- Dockerfile
+++ Dockerfile
@@ -17,9 +17,24 @@ RUN apt-get install -y mariadb-server mariadb-client
 
 RUN apt-get install openssl
 
+COPY ./srcs ./
+
+RUN mkdir var/www/localhost \
+	&& mkdir /etc/nginx/ssl
+
+RUN wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-english.tar.gz \
+	&& tar -xzf phpMyAdmin-5.0.2-english.tar.gz \
+	&& mv phpMyAdmin-5.0.2-english /var/www/localhost/phpmyadmin \
+	&& mv ./config.inc.php var/www/localhost/phpmyadmin \
+	&& rm -rf phpMyAdmin-5.0.2-english.tar.gz
+
+RUN wget https://wordpress.org/latest.tar.gz \
+    && tar -xzf latest.tar.gz \
+	&& mv wordpress /var/www/localhost/wordpress \
+    && mv ./wp-config.php /var/www/localhost/wordpress \
+    && rm -rf latest.tar.gz
+
 EXPOSE 80
 EXPOSE 443
-
-COPY ./srcs ./
 
 CMD bash start.sh
--- srcs/start.sh
+++ srcs/start.sh
@@ -1,22 +1,12 @@
-#this is a shell scrip that should run a bunch of things
-
-
-# what happens if i do echo "<?php phpinfo(); ?>" >> /var/www/localhost/index.php regardless of on or off ?
-#nginx
-mkdir var/www/localhost
+#Sets up right nginx.config file depending on if autoindexing is on or off
 if [ "$AUTOINDEX" = "off" ]	;
 then cp ./nginx.auto-off.config /etc/nginx/sites-available/default \
 	&& touch /var/www/localhost/index.php \
-	&& echo "<?php phpinfo(); ?>" >> /var/www/localhost/index.php && echo "off";
-else cp ./nginx.auto-on.config /etc/nginx/sites-available/default && echo "on"; fi
+	&& echo "<?php phpinfo(); ?>" >> /var/www/localhost/index.php && echo "Autoindex off";
+else cp ./nginx.auto-on.config /etc/nginx/sites-available/default && echo "Autoindex on"; fi
 
-
-#making a key or something for ssl
-mkdir /etc/nginx/ssl
+#making ssl key
 openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=erlazo/CN=localhost"
-
-#touch /var/www/localhost/index.php
-#echo "<?php phpinfo(); ?>" >> /var/www/localhost/index.php
 
 #MYSQL
 service mysql start
@@ -25,26 +15,10 @@ echo "CREATE USER wp_user@localhost IDENTIFIED BY 'password';" | mysql -u root
 echo "GRANT ALL ON wordpress.* TO 'wp_user'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
-# setting up phpmyadmin
-wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-english.tar.gz
-# -v is verbose
-tar -zxf phpMyAdmin-4.9.0.1-english.tar.gz
-mv phpMyAdmin-4.9.0.1-english /var/www/localhost/phpmyadmin
-#now adding the phpmyadmin config file
-mv ./config.inc.php var/www/localhost/phpmyadmin
-#chmod 660 /var/www/localhost/phpmyadmin/config.inc.php
-#chown -R www-data:www-data /var/www/localhost/phpmyadmin
 #now we can start php
 service php7.3-fpm start
 echo "GRANT ALL ON *.* TO 'erlazo'@'localhost' IDENTIFIED BY '123';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
-
-#now wordpress
-wget https://wordpress.org/latest.tar.gz
-tar -xf latest.tar.gz
-mkdir var/www/localhost/wordpress
-cp -a wordpress/. /var/www/localhost/wordpress
-mv ./wp-config.php /var/www/localhost/wordpress
 
 chown -R www-data:www-data /var/www/*
 chmod -R 755 /var/www/*
@@ -53,13 +27,4 @@ chmod -R 755 /var/www/*
 service nginx start
 service mysql restart
 service php7.3-fpm restart
-#sleep infinity
 bash
-
-
-
-
-
-
-
-
